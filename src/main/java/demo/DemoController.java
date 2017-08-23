e0838fc1d5d382d8b15d1a4887b0dc97f8ff3b8a
U620189 <U620189@RDBX-AG000085.bxo.local>
2017-08-23 18:16:59 +0900

    edit url

--- src/main/java/demo/DemoController.java
+++ src/main/java/demo/DemoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @EnableAutoConfiguration
 public class DemoController {
-	@RequestMapping("/")
+	@RequestMapping("/demo")
 	@ResponseBody
 	public String home() {
 		return "Hello, Spring Boot Sample Application!";
--- src/main/java/test/HelloController.java
+++ src/main/java/test/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @EnableAutoConfiguration
 public class HelloController {
-	@RequestMapping("/")
+	@RequestMapping("/test")
 	@ResponseBody
 	public String home() {
 		return "Hello, Spring Boot Sample Application!";
