0ed51585b65b68b2867b25ba5bd6ad9ca2d04163
U620189 <U620189@RDBX-AG000085.bxo.local>
2017-08-23 18:23:35 +0900

    edit url

--- src/main/java/demo/DemoController.java
+++ src/main/java/demo/DemoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @EnableAutoConfiguration
 public class DemoController {
-	@RequestMapping("/demo")
+	@RequestMapping("/")
 	@ResponseBody
 	public String home() {
 		return "Hello, Spring Boot Sample Application!";
